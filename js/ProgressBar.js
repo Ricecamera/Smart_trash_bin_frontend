7bfaa802914f8ed7bb48c3bcb2786e05f80375fa
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-05-31 03:06:27 +0700

    Gauge can change  color now

--- js/ProgressBar.js
+++ js/ProgressBar.js
@@ -8,8 +8,7 @@ class ProgressBar {
         this.valueElem = $(label).get(0);
         this.fillElem = $(`${element}>.trash-icon-fill`).get(0);
         this.unfillElem = $(`${element}>.trash-icon-unfill`).get(0);
-        /*console.log(this.fillElem);
-        console.log(this.unfillElem);
+        /*console.log(this.unfillElem);
         console.log(this.valueElem);*/
 
         this.setValue(initialValue);
@@ -29,19 +28,20 @@ class ProgressBar {
     update() {
         const percentage = this.value + "%";
         const unfillPercentage = (100 - this.value) + "%";
+        const colorChanger = this.fillElem.querySelector('path');
 
         this.valueElem.innerHTML = percentage;
         this.unfillElem.style.height = unfillPercentage;
 
         if(this.value > 75) {
-            this.fillElem.style.color = dangerColor;
+            colorChanger.style.fill = dangerColor;
             this.valueElem.style.color = dangerColor;
         }
         else if(this.value > 45) {
-            this.fillElem.style.color = warningColor;
+            colorChanger.style.fill = warningColor;
             this.valueElem.style.color = warningColor;
         } else {
-            this.fillElem.style.color = safeColor;
+            colorChanger.style.fill = safeColor;
             this.valueElem.style.color = safeColor;
         }
     }
